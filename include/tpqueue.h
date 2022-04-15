24100cde4f1cc015002209dac3fe4fb8901ea35f
Daria-Skvortsova <99176875+Daria-Skvortsova@users.noreply.github.com>
2022-04-15 14:19:23 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -10,32 +10,30 @@ class TPQueue {
     int last;
 
  public:
-  TPQueue() :first(0), last(0) { }
-  void push(T x) {
-    int j = 0;
-    T value = x;
-    for (int i = first; i < size; i++) {
-      j = last - 1;
-      while ((j >= first) && (arr[(j + 1) % size].prior < value.prior)) {
-        if ((arr[(j) % size].prior == value.prior)) {
-          arr[(j + 1) % size] = value;
-          j++;
-          break;
+    TPQueue() :first(0), last(0) { }
+    void push(T x) {
+        int j = 0;
+        T value = x;
+        for (int i = first; i < size; i++) {
+            j = last++ - 1;
+            while ((j >= first) && (arr[(j) % size].prior < value.prior)) {
+                if (arr[j % size].prior == value.prior) {
+                    arr[(j++) % size] = value;
+                    break;
+                }
+                arr[(j + 1) % size] = arr[j % size];
+                j--;
+            }
+            arr[(j++ + 1) % size] = value;
+            break;
         }
-        j--;
-        arr[(j + 1) % size] = arr[j % size];
-      }
-      arr[(j + 1) % size] = value;
-      last++;
-      break;
     }
-  }
-  T pop() {
-    return arr[(first++) % size];
-  }
+    T pop() {
+        return arr[(first++) % size];
+    }
 };
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 #endif  // INCLUDE_TPQUEUE_H_
